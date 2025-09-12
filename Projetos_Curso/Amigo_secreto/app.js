0fa8bcd9b2e2d4286cb3be5f7693668789bd21e3
Patricia <patriciahm7@hotmail.com>
2025-09-12 17:32:19 -0300

    Update app.js

--- Projetos_Curso/Amigo_secreto/app.js
+++ Projetos_Curso/Amigo_secreto/app.js
@@ -1,1 +1,8 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+
+
+
+// Array para guardar os nomes dos amigos
+
+let nomesAmigos = [];
